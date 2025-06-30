a90b33b302d46ca59bc53f25e4d44c2081e0d495
JingyangQiao <159891886+JingyangQiao@users.noreply.github.com>
2025-07-01 00:48:47 +0800

    Update qwen_train_moe.sh

--- scripts/train/qwen_train_moe.sh
+++ scripts/train/qwen_train_moe.sh
@@ -1,4 +1,4 @@
-NPROC_PER_NODE=2 \
+PEFT_LAYER_TYPE=moelora NPROC_PER_NODE=2 \
 CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --add_version False \
     --deepspeed zero0 \
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --dataset ./datasets/2022-train.json --split_dataset_ratio 0 --max_length 4096 --train_dataloader_shuffle False --dataloader_num_workers 4 --attn_impl flash_attn \
     --lora_rank 64 --init_weights True --per_device_train_batch_size 4 --num_train_epochs 20 --learning_rate 1e-4 --gradient_accumulation_steps 1 --eval_steps 50 --output_dir ./output/qwen2_5/moe/2022
 
-NPROC_PER_NODE=2 \
+PEFT_LAYER_TYPE=moelora NPROC_PER_NODE=2 \
 CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --add_version False \
     --deepspeed zero0 \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --lora_rank 64 --init_weights True --per_device_train_batch_size 4 --num_train_epochs 20 --learning_rate 1e-4 --gradient_accumulation_steps 1 --eval_steps 50 --output_dir ./output/qwen2_5/moe/2023 \
     --adapters ./output/qwen2_5/moe/2022/checkpoint-560
 
-NPROC_PER_NODE=2 \
+PEFT_LAYER_TYPE=moelora NPROC_PER_NODE=2 \
 CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --add_version False \
     --deepspeed zero0 \
@@ -27,7 +27,7 @@ CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --lora_rank 64 --init_weights True --per_device_train_batch_size 4 --num_train_epochs 20 --learning_rate 1e-4 --gradient_accumulation_steps 1 --eval_steps 50 --output_dir ./output/qwen2_5/moe/2024 \
     --adapters ./output/qwen2_5/moe/2023/checkpoint-560
 
-NPROC_PER_NODE=2 \
+PEFT_LAYER_TYPE=moelora NPROC_PER_NODE=2 \
 CUDA_VISIBLE_DEVICES=0,1 swift sft --torch_dtype bfloat16 \
     --add_version False \
     --deepspeed zero0 \
