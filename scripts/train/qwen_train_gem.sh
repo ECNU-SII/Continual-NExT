ad3590e669da6c22113fcf16f67d1cb289d9dce2
WeichengMeng <92626572+fookeof@users.noreply.github.com>
2025-07-02 16:45:53 +0800

    Update qwen_train_gem.sh

--- scripts/train/qwen_train_gem.sh
+++ scripts/train/qwen_train_gem.sh
@@ -40,7 +40,8 @@ CUDA_VISIBLE_DEVICES=0,1  swift sft --torch_dtype 'bfloat16' \
     --gradient_accumulation_steps '1' \
     --eval_steps '50' \
     --output_dir './output/qwen2_5/gem/2023' \
-    --adapters ./output/qwen2_5/gem/2022/checkpoint-560 \
+    --resume_from_checkpoint ./output/qwen2_5/gem/2022/checkpoint-560 \
+    --resume_only_model True \
     --GEM True \
     --GEM_memory_strength 0.5 \
     --GEM_replay_ratio 0.2 \
@@ -68,7 +69,8 @@ CUDA_VISIBLE_DEVICES=0,1  swift sft --torch_dtype 'bfloat16' \
     --gradient_accumulation_steps '1' \
     --eval_steps '50' \
     --output_dir './output/qwen2_5/gem/2024' \
-    --adapters ./output/qwen2_5/gem/2023/checkpoint-660 \
+    --resume_from_checkpoint ./output/qwen2_5/gem/2023/checkpoint-660 \
+    --resume_only_model True \
     --GEM True \
     --GEM_memory_strength 0.5 \
     --GEM_replay_ratio 0.2 \
@@ -96,7 +98,8 @@ CUDA_VISIBLE_DEVICES=0,1  swift sft --torch_dtype 'bfloat16' \
     --gradient_accumulation_steps '1' \
     --eval_steps '50' \
     --output_dir './output/qwen2_5/gem/2025' \
-    --adapters ./output/qwen2_5/gem/2024/checkpoint-940 \
+    --resume_from_checkpoint ./output/qwen2_5/gem/2024/checkpoint-940 \
+    --resume_only_model True \
     --GEM True \
     --GEM_memory_strength 0.5 \
     --GEM_replay_ratio 0.2 \
